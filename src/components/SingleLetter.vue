0a3761e6ac31596c7412fbfb08703a95804fff85
Omri Bar-Zik <omri@bar-zik.com>
2022-09-09 11:24:42 +0300

    init commit

---
+++ src/components/SingleLetter.vue
@@ -0,0 +1,25 @@
+<script>
+import { ref } from "vue";
+import theHumanKeyboard from "../assets/theHumanKeyboard.jpeg";
+export default {
+  mounted() {
+    const ctx = this.letterCanvas.getContext("2d");
+
+    ctx.drawImage(this.img, 0, 0);
+  },
+  data() {
+    const img = ref(document.createElement("img"));
+    img.value.src = theHumanKeyboard;
+
+    return {
+      /** @type {import("vue").Ref<HTMLCanvasElement>} */
+      letterCanvas: ref(null),
+      img,
+    };
+  },
+};
+</script>
+
+<template>
+  <canvas ref="letterCanvas"></canvas>
+</template>
